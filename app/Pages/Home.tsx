25bf17d27c9152a9ee2ce130a3d2961fc0122cbc
MarinaKononenko <macdanieltatenda00@gmail.com>
2024-12-19 04:06:55 +0300

    Home update List/Note🎉

--- app/Pages/Home.tsx
+++ app/Pages/Home.tsx
@@ -1,8 +1,14 @@
 import { Input } from "postcss";
 import React from "react";
-import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Checkbox from "@mui/material/Checkbox";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 function Home() {
   return (
@@ -13,21 +19,93 @@ function Home() {
         {/* header*/}
         <div className="flex flex-start w-full  gap-[18px]">
           <div className="flex flex-row justify-between w-[80%] rounded-[5px] border-blue-300 border-2 px-[16px] py-[8px]">
-            <input placeholder="Search note..."  className="focus:outline-none w-[75%]"/>
+            <input
+              placeholder="Search note..."
+              className="focus:outline-none w-[75%]"
+            />
             <SearchSharpIcon color="primary" sx={{ fontSize: 22 }} />
           </div>
           <div className="bg-blue-500 w-[11%] flex flex-row justify-center items-center gap-[10px] rounded-[5px] text-white">
-            <h1 className="font-kanit text-[18px] font-medium uppercase">ALL</h1>
-            <KeyboardArrowDownIcon fontSize="small"/>
+            <h1 className="font-kanit text-[18px] font-medium uppercase">
+              ALL
+            </h1>
+            <KeyboardArrowDownIcon fontSize="small" />
           </div>
           <div className="bg-blue-500 w-[5%] rounded-[5px] flex justify-center items-center text-white ">
-            <DarkModeOutlinedIcon sx={{ fontSize: 32 }}/>
+            <DarkModeOutlinedIcon sx={{ fontSize: 32 }} />
           </div>
         </div>
       </div>
 
-      {/*List todos*/}
-      <div>Todo list</div>
+      {/*Body*/}
+      <div className="flex flex-row w-full">
+        {/*List*/}
+        <div className="w-[88%] ml-24 mt-[30px] flex flex-col gap-[17px]">
+          {/*Note*/}
+          <div className="flex flex-row justify-between items-center py-[17px] border-b-2 border-blue-300">
+            <div className="flex flex-row items-center justify-center gap-[10px]">
+              <Checkbox
+                {...label}
+                sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+              />
+              <p className="font-kanit text-[20px] font-medium text-center">
+                Note #1
+              </p>
+            </div>
+            <div className="flex flex-row gap-[10px]">
+              <CreateOutlinedIcon color="inherit" sx={{ fontSize: 20 }} />
+              <DeleteOutlineOutlinedIcon
+                color="inherit"
+                sx={{ fontSize: 20 }}
+              />
+            </div>
+          </div>
+          {/*Note2*/}
+          <div className="flex flex-row justify-between items-center py-[17px] border-b-2 border-blue-300">
+            <div className="flex flex-row items-center justify-center gap-[10px]">
+              <Checkbox
+                {...label}
+                defaultChecked
+                sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+              />
+              <p className="font-kanit text-[20px] font-medium text-center">
+                Note #2
+              </p>
+            </div>
+            <div className="flex flex-row gap-[10px]">
+              <CreateOutlinedIcon color="inherit" sx={{ fontSize: 20 }} />
+              <DeleteOutlineOutlinedIcon
+                color="inherit"
+                sx={{ fontSize: 20 }}
+              />
+            </div>
+          </div>
+          {/*Note3*/}
+          <div className="flex flex-row justify-between items-center py-[17px]">
+            <div className="flex flex-row items-center justify-center gap-[10px]">
+              <Checkbox
+                {...label}
+                sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+              />
+              <p className="font-kanit text-[20px] font-medium text-center">
+                Note #3
+              </p>
+            </div>
+            <div className="flex flex-row gap-[10px]">
+              <CreateOutlinedIcon color="inherit" sx={{ fontSize: 20 }} />
+              <DeleteOutlineOutlinedIcon
+                color="inherit"
+                sx={{ fontSize: 20 }}
+              />
+            </div>
+          </div>
+        </div>
+        <AddCircleIcon
+          sx={{ fontSize: 50 }}
+          color="primary"
+          className="ml-12 mt-[450px]"
+        />
+      </div>
     </div>
   );
 }
